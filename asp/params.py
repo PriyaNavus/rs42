ec837e2375d8b0145d71310d971186411c29492a
Priyadharshini Venkatraj <venkatraj@uni-potsdam.de>
2026-07-03 16:09:53 +0200

    Initial ASP Flatland railway scheduling project

--- asp/params.py
+++ asp/params.py
@@ -1,5 +1,14 @@
 #primary=['asp/graph_based/actions.lp','asp/graph_based/graph.lp','asp/graph_based/traverse.lp']
 #primary=['asp/action_based/actions.lp','asp/action_based/pathfinding.lp','asp/action_based/transitions.lp']
 
-primary=['asp/test.lp']
-secondary=[]
+#primary=['asp/test.lp']
+#secondary=[]
+
+primary = [
+    "asp/custom/connection.lp",
+    "asp/custom/encoding.lp",
+    "asp/custom/waypoint.lp",
+    "asp/custom/visual.lp"
+]
+
+secondary = []
